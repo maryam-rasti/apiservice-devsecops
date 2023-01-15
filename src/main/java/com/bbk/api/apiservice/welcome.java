0274306c1c63c5af28b285dd931943ef6bc88475
603-2 <603-2@bibf.com>
2023-01-15 12:50:09 +0300

    first commit

---
+++ src/main/java/com/bbk/api/apiservice/welcome.java
@@ -0,0 +1,14 @@
+package com.bbk.api.apiservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class welcome {
+	
+	@GetMapping("/welcome")
+	public String welcome() {
+		return"welcome to DevSecOps class";
+	}
+
+}
